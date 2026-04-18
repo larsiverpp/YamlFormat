305a33498d1b719ac97dd73dec8176584047e908
Lars Iversen <LAIVE@danskebank.dk>
2026-04-18 18:10:50 +0200

    Update _Development

---
+++ _Development/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Minor Code Smell", "S1192:String literals should not be duplicated", Justification = "Bad rule")]
+[assembly: SuppressMessage("Minor Code Smell", "S6444:Pass a timeout to limit the execution time", Justification = "We are not using complex regex patterns")]
